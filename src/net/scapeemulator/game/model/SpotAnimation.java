91d804f890b60d0b8f0731378725e5c81d59cc91
Vincent - IfearI - FearTheDust <vinni_tw@hotmail.com>
2014-06-15 20:30:33 +0200

    Added documentation to SpotAnimation.

--- src/net/scapeemulator/game/model/SpotAnimation.java
+++ src/net/scapeemulator/game/model/SpotAnimation.java
@@ -1,33 +1,64 @@
 package net.scapeemulator.game.model;
 
+/**
+ * Represents a certain Animation with an Id, delay and height.
+ * All its attributes are immutable.
+ * 
+ */
 public final class SpotAnimation {
 
 	private final int id, delay, height;
 
+        /**
+         * Creates a {@link SpotAnimation} with the provided id, 0 delay and 0 height.
+         * @param id The id of the {@link SpotAnimation}.
+         */
 	public SpotAnimation(int id) {
 		this(id, 0, 0);
 	}
 
+        /**
+         * Creates a {@link SpotAnimation} with the provided id, delay and 0 height.
+         * @param id The id of the {@link SpotAnimation}.
+         * @param delay The delay of the {@link SpotAnimation}.
+         */
 	public SpotAnimation(int id, int delay) {
 		this(id, delay, 0);
 	}
 
+        /**
+         * Creates a {@link SpotAnimation} with the provided id, delay and height.
+         * @param id The id of the {@link SpotAnimation}.
+         * @param delay The delay of the {@link SpotAnimation}.
+         * @param height The height of this {@link SpotAnimation}.
+         */
 	public SpotAnimation(int id, int delay, int height) {
 		this.id = id;
 		this.delay = delay;
 		this.height = height;
 	}
 
+        /**
+         * Gets the id of this {@link SpotAnimation}.
+         * @return 
+         */
 	public int getId() {
 		return id;
 	}
 
+        /**
+         * Gets the delay of this {@link SpotAnimation}.
+         * @return 
+         */
 	public int getDelay() {
 		return delay;
 	}
 
+        /**
+         * Gets the height of this {@link SpotAnimation}.
+         * @return 
+         */
 	public int getHeight() {
 		return height;
 	}
-
 }
