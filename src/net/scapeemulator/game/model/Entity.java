fc49129cf08b33f81840c76aac93a8a67caf45e3
Vincent - IfearI - FearTheDust <vinni_tw@hotmail.com>
2014-06-15 20:31:00 +0200

    Added documentation to Entity.

--- src/net/scapeemulator/game/model/Entity.java
+++ src/net/scapeemulator/game/model/Entity.java
@@ -1,13 +1,24 @@
 package net.scapeemulator.game.model;
 
+/**
+ * An Entity which has a certain Position.
+ */
 public abstract class Entity {
 
 	protected Position position;
 
+        /**
+         * Retrieve the position of this {@link Entity}
+         * @return 
+         */
 	public Position getPosition() {
 		return position;
 	}
 
+        /**
+         * Set the {@link Position} of this {@link Entity} to position.
+         * @param position The {@link Position} to set it to.
+         */
 	public void setPosition(Position position) {
 		this.position = position;
 	}
