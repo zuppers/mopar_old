2d3a4597872a193bf02a90b0267ff23e1a2239ff
Vincent - IfearI - FearTheDust <vinni_tw@hotmail.com>
2014-06-17 09:37:30 +0200

    Added documentation to game.model.MobList

--- src/net/scapeemulator/game/model/MobList.java
+++ src/net/scapeemulator/game/model/MobList.java
@@ -5,94 +5,150 @@ import java.util.NoSuchElementException;
 
 import net.scapeemulator.game.model.mob.Mob;
 
+/**
+ * A {@link MobList} can store a certain amount of a certain {@link Mob} type.
+ * It provides a way to retrieve, add and remove as well as an {@link MobListIterator}.
+ * 
+ * @author Graham?
+ * @param <T> The type of {@link Mob} this {@link MobList} should store.
+ */
 public final class MobList<T extends Mob> implements Iterable<T> {
 
-	private final Mob[] mobs;
-	private int size = 0;
+    private final Mob[] mobs;
+    private int size = 0;
 
-	private class MobListIterator implements Iterator<T> {
+    /**
+     * Create a {@link MobList} with a certain maximum capacity.
+     * @param capacity The capacity of this {@link MobList}.
+     */
+    public MobList(int capacity) {
+        mobs = new Mob[capacity];
+    }
 
-		private int index = 0;
+    /**
+     * The size of the list, the amount of {@link Mob}'s stored.
+     * @return 
+     */
+    public int getSize() {
+        return size;
+    }
 
-		@Override
-		public boolean hasNext() {
-			for (int i = index; i < mobs.length; i++) {
-				if (mobs[i] != null)
-					return true;
-			}
+    /**
+     * Add mob to this {@link MobList}.
+     * Seeks the first empty spot in the list and fills it up.
+     * The Id of mob will be updated as well. ({@link Mob#setId(int)})
+     * @param mob The element to add.
+     * @return True when successfully added, false otherwise.
+     */
+    public boolean add(T mob) {
+        for (int id = 0; id < mobs.length; id++) {
+            if (mobs[id] == null) {
+                mobs[id] = mob;
+                size++;
 
-			return false;
-		}
-
-		@SuppressWarnings("unchecked")
-		@Override
-		public T next() {
-			for (; index < mobs.length; index++) {
-				if (mobs[index] != null)
-					return (T) mobs[index++];
-			}
-
-			throw new NoSuchElementException();
-		}
-
-		@SuppressWarnings("unchecked")
-		@Override
-		public void remove() {
-			if (index == 0 || mobs[index - 1] == null)
-				throw new IllegalStateException();
-
-			MobList.this.remove((T) mobs[index - 1]);
-		}
-
-	}
-
-	public MobList(int capacity) {
-		mobs = new Mob[capacity];
-	}
-
-	public int getSize() {
-		return size;
-	}
-	
-	public boolean add(T mob) {
-		for (int id = 0; id < mobs.length; id++) {
-			if (mobs[id] == null) {
-				mobs[id] = mob;
-				size++;
-
-				mob.setId(id + 1);
-				return true;
-			}
-		}
-
-		return false;
-	}
-        
-        @SuppressWarnings("unchecked")
-		public T get(int index) {
-            if(index <= 0 || index >= mobs.length + 1) {
-                return null;
+                mob.setId(id + 1);
+                return true;
             }
-            
-            return (T) mobs[index - 1];
         }
 
-	public void remove(T mob) {
-		int id = mob.getId();
-		assert id != 0;
+        return false;
+    }
 
-		id--;
-		assert mobs[id] == mob;
+    /**
+     * Gets the element on the index specified.
+     * @param index The index where to find retrieve the element from.
+     * @return The element on that index. 
+     * null is returned when index exceeds the boundaries.
+     */
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        if (index <= 0 || index >= mobs.length + 1) {
+            return null;
+        }
 
-		mobs[id] = null;
-		size--;
+        return (T) mobs[index - 1];
+    }
 
-		mob.resetId();
-	}
+    /**
+     * Remove the element from this {@link MobList}.
+     * @param mob The element to remove.
+     */
+    public void remove(T mob) {
+        int id = mob.getId();
+        assert id != 0;
 
-	@Override
-	public Iterator<T> iterator() {
-		return new MobListIterator();
-	}
+        id--;
+        assert mobs[id] == mob;
+
+        mobs[id] = null;
+        size--;
+
+        mob.resetId();
+    }
+
+    /**
+     * Gets a new {@link MobListIterator} to iterate over this {@link MobList}'s elements.
+     * @return 
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new MobListIterator();
+    }
+
+    /**
+     * An {@link Iterator} to iterate over elements the {@link MobList} contains.
+     */
+    private class MobListIterator implements Iterator<T> {
+
+        private int index = 0;
+
+        /**
+         * Gets whether there is another element left for {@link next()}.
+         * @return 
+         */
+        @Override
+        public boolean hasNext() {
+            for (int i = index; i < mobs.length; i++) {
+                if (mobs[i] != null) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /**
+         * Gets the next element.
+         * @return 
+         * @throws NoSuchElementException When there is no next element.
+         * @see hasNext()
+         */
+        @SuppressWarnings("unchecked")
+        @Override
+        public T next() {
+            for (; index < mobs.length; index++) {
+                if (mobs[index] != null) {
+                    return (T) mobs[index++];
+                }
+            }
+
+            throw new NoSuchElementException();
+        }
+
+        /**
+         * Remove the current element.
+         * @throws IllegalStateException When there is no current element.
+         */
+        @SuppressWarnings("unchecked")
+        @Override
+        public void remove() {
+            if (index == 0 || mobs[index - 1] == null) {
+                throw new IllegalStateException();
+            }
+
+            MobList.this.remove((T) mobs[index - 1]);
+        }
+
+    }
 
 }
