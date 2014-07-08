e45970cffbab18806a6938e8c9785cfaa6cb698c
Davidi2 <davidtheinsley@comcast.net>
2014-07-07 19:19:58 -0700

    Fixed bone burying design

---
+++ src/net/scapeemulator/game/model/player/skills/prayer/BuryBoneAction.java
@@ -0,0 +1,45 @@
+package net.scapeemulator.game.model.player.skills.prayer;
+
+import net.scapeemulator.game.model.mob.Animation;
+import net.scapeemulator.game.model.player.Player;
+import net.scapeemulator.game.model.player.SlottedItem;
+import net.scapeemulator.game.model.player.skills.Skill;
+import net.scapeemulator.game.task.Action;
+
+/**
+ * @author David Insley
+ */
+public class BuryBoneAction extends Action<Player> {
+
+    private static final Animation BURY_ANIMATION = new Animation(827);
+
+    private final Bone bone;
+    private final SlottedItem item;
+
+    private boolean started;
+
+    public BuryBoneAction(Player player, Bone bone, SlottedItem item) {
+        super(player, 3, true);
+        this.bone = bone;
+        this.item = item;
+    }
+
+    @Override
+    public void execute() {
+        if (!started) {
+            mob.getWalkingQueue().reset();
+            mob.setActionsBlocked(true);
+            mob.getInventory().remove(item);
+            mob.sendMessage("You dig a hole in the ground...");
+            mob.playAnimation(BURY_ANIMATION);
+            mob.getSkillSet().addExperience(Skill.PRAYER, bone.getXp());
+            started = true;
+        } else {
+            mob.sendMessage("You bury the bones.");
+            mob.setActionsBlocked(false);
+            stop();
+        }
+
+    }
+
+}
--- src/net/scapeemulator/game/model/player/skills/prayer/BuryBoneHandler.java
+++ src/net/scapeemulator/game/model/player/skills/prayer/BuryBoneHandler.java
@@ -1,19 +1,13 @@
 package net.scapeemulator.game.model.player.skills.prayer;
 
 import net.scapeemulator.game.item.ItemInteractHandler;
-import net.scapeemulator.game.model.World;
-import net.scapeemulator.game.model.mob.Animation;
 import net.scapeemulator.game.model.player.Player;
 import net.scapeemulator.game.model.player.SlottedItem;
-import net.scapeemulator.game.model.player.skills.Skill;
-import net.scapeemulator.game.task.Action;
 
 /**
  * @author David Insley
  */
 public class BuryBoneHandler extends ItemInteractHandler {
-
-    private static final Animation BURY_ANIMATION = new Animation(827);
 
     private final Bone bone;
 
@@ -24,19 +18,7 @@ public class BuryBoneHandler extends ItemInteractHandler {
 
     @Override
     public void handle(Player player, SlottedItem item) {
-        player.getWalkingQueue().reset();
-        player.setActionsBlocked(true);
-        World.getWorld().getTaskScheduler().schedule(new Action<Player>(player, 3, false) {
-            public void execute() {
-                mob.sendMessage("You bury the bones.");
-                mob.setActionsBlocked(false);
-                stop();
-            }
-        });
-        player.getInventory().remove(item);
-        player.sendMessage("You dig a hole in the ground...");
-        player.playAnimation(BURY_ANIMATION);
-        player.getSkillSet().addExperience(Skill.PRAYER, bone.getXp());
+        player.startAction(new BuryBoneAction(player, bone, item));
     }
 
 }
