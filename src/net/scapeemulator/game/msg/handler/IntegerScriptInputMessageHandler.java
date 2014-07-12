33d483f07821931972a8ffbdfbd2441203c4ddc0
Davidi2 <davidtheinsley@comcast.net>
2014-07-11 22:40:49 -0700

    Fixed formatting

--- src/net/scapeemulator/game/msg/handler/IntegerScriptInputMessageHandler.java
+++ src/net/scapeemulator/game/msg/handler/IntegerScriptInputMessageHandler.java
@@ -6,16 +6,16 @@ import net.scapeemulator.game.msg.impl.IntegerScriptInputMessage;
 
 public final class IntegerScriptInputMessageHandler extends MessageHandler<IntegerScriptInputMessage> {
 
-	@Override
-	public void handle(Player player, IntegerScriptInputMessage message) {
-	    if(message.getValue() < 1) {
-	        return;
-	    }
-		if(player.getScriptInput().getIntegerInputListener() != null) {
-			player.getScriptInput().getIntegerInputListener().inputReceived(message.getValue());
-		} else {
-			System.out.println("Unhandled script input: " + message.getValue());
-		}
-	}
+    @Override
+    public void handle(Player player, IntegerScriptInputMessage message) {
+        if (message.getValue() < 1) {
+            return;
+        }
+        if (player.getScriptInput().getIntegerInputListener() != null) {
+            player.getScriptInput().getIntegerInputListener().inputReceived(message.getValue());
+        } else {
+            System.out.println("Unhandled script input: " + message.getValue());
+        }
+    }
 
 }
