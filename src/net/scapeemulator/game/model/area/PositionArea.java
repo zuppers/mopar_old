f8f212bcd579bc0fd46efc1412426304f0a8a8fd
Davidi2 <davidtheinsley@comcast.net>
2014-07-12 21:27:02 -0700

    Added PositionArea, an Area wrapping for a single Position to use for DistancedActions.

---
+++ src/net/scapeemulator/game/model/area/PositionArea.java
@@ -0,0 +1,28 @@
+package net.scapeemulator.game.model.area;
+
+import net.scapeemulator.game.model.Position;
+
+/**
+ * Area wrapping for Position
+ * 
+ * @author David Insley
+ */
+public final class PositionArea extends Area {
+
+    private final Position position;
+
+    public PositionArea(Position position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean withinAreaPadding(int x, int y, int padding) {
+        return x >= (position.getX() - padding) && x <= (position.getX() + padding) && y >= (position.getY() - padding) && y <= (position.getY() + padding);
+    }
+
+    @Override
+    public Position randomPosition(int height) {
+        return new Position(position.getX(), position.getY(), height);
+    }
+
+}
