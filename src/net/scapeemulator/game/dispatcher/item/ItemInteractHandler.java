c9b1bd50687d27c34c761b3706ffcfb2cba6be22
Davidi2 <davidtheinsley@comcast.net>
2014-08-03 18:54:42 -0700

    Documented ItemInteractHandler to make sure verification is more clear

--- src/net/scapeemulator/game/dispatcher/item/ItemInteractHandler.java
+++ src/net/scapeemulator/game/dispatcher/item/ItemInteractHandler.java
@@ -19,5 +19,15 @@ public abstract class ItemInteractHandler {
         return itemId;
     }
 
+    /**
+     * Handles the item interaction. The handler must verify that the inventory is the correct
+     * inventory, but the dispatcher <i>will</i> verify the item exists. For example, eating should
+     * not happen in the bank, so verify the Inventory argument matches the players active
+     * inventory.
+     * 
+     * @param inventory Inventory the item was interacted with from
+     * @param player Player the interacted with the item
+     * @param item represents the id, amount, and slot id of the item interacted with
+     */
     public abstract void handle(Inventory inventory, Player player, SlottedItem item);
 }
