49d4a356e45a539ede9458377920e40bcc50598b
Davidi2 <davidtheinsley@comcast.net>
2014-10-05 14:05:30 -0700

    Height modulus for teleport update descriptor as well

--- src/net/scapeemulator/game/update/TeleportPlayerDescriptor.java
+++ src/net/scapeemulator/game/update/TeleportPlayerDescriptor.java
@@ -7,26 +7,26 @@ import net.scapeemulator.game.net.game.GameFrameBuilder;
 
 public final class TeleportPlayerDescriptor extends PlayerDescriptor {
 
-	public TeleportPlayerDescriptor(Player player, int[] tickets) {
-		super(player, tickets);
-	}
+    public TeleportPlayerDescriptor(Player player, int[] tickets) {
+        super(player, tickets);
+    }
 
-	@Override
-	public void encodeDescriptor(PlayerUpdateMessage message, GameFrameBuilder builder, GameFrameBuilder blockBuilder) {
-		Position lastKnownRegion = message.getLastKnownRegion();
-		Position position = message.getPosition();
+    @Override
+    public void encodeDescriptor(PlayerUpdateMessage message, GameFrameBuilder builder, GameFrameBuilder blockBuilder) {
+        Position lastKnownRegion = message.getLastKnownRegion();
+        Position position = message.getPosition();
 
-		int x = position.getLocalX(lastKnownRegion.getRegionX());
-		int y = position.getLocalY(lastKnownRegion.getRegionY());
-		int height = position.getHeight();
+        int x = position.getLocalX(lastKnownRegion.getRegionX());
+        int y = position.getLocalY(lastKnownRegion.getRegionY());
+        int height = position.getHeight() % 4;
 
-		builder.putBits(1, 1);
-		builder.putBits(2, 3);
-		builder.putBits(7, y);
-		builder.putBits(1, 1);
-		builder.putBits(2, height);
-		builder.putBits(1, isBlockUpdatedRequired() ? 1 : 0);
-		builder.putBits(7, x);
-	}
+        builder.putBits(1, 1);
+        builder.putBits(2, 3);
+        builder.putBits(7, y);
+        builder.putBits(1, 1);
+        builder.putBits(2, height);
+        builder.putBits(1, isBlockUpdatedRequired() ? 1 : 0);
+        builder.putBits(7, x);
+    }
 
 }
