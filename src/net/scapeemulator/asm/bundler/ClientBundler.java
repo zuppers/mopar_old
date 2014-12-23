59b51d733d4f65bbfc6372b81cfe5c8f729afc45
Virus <Virus@David-PC>
2014-12-22 22:05:12 -0800

    Fixed paths

--- src/net/scapeemulator/asm/bundler/ClientBundler.java
+++ src/net/scapeemulator/asm/bundler/ClientBundler.java
@@ -28,11 +28,11 @@ public final class ClientBundler {
 	public void bundle() throws IOException, NoSuchAlgorithmException {
 		/* unpack source files */
 		logger.info("Starting client bundler...");
-		Application client = Application.unpackJar(new File("data/runescape.jar"));
-		Application glClient = Application.unpack200(new File("data/runescape_gl.pack200"));
-		Application loader = Application.unpackJar(new File("data/loader.jar"));
-		Application glLoader = Application.unpackJar(new File("data/loader_gl.jar"));
-		Application jogl = Application.unpackJar(new File("data/jogl.jar"));
+		Application client = Application.unpackJar(new File("data/asm/runescape.jar"));
+		Application glClient = Application.unpack200(new File("data/asm/runescape_gl.pack200"));
+		Application loader = Application.unpackJar(new File("data/asm/loader.jar"));
+		Application glLoader = Application.unpackJar(new File("data/asm/loader_gl.jar"));
+		Application jogl = Application.unpackJar(new File("data/asm/jogl.jar"));
 
 		/* update client RSA keys */
 		logger.info("Transforming RSA keys...");
@@ -48,10 +48,10 @@ public final class ClientBundler {
 
 		/* write everything but the loaders back out */
 		logger.info("Bundling clients and libraries...");
-		File clientOut = new File("../game/data/www/runescape.pack200");
-		File glClientOut = new File("../game/data/www/runescape_gl.pack200");
-		File joglOut = new File("../game/data/www/jogl.pack200");
-		client.packJar(new File("../game/data/www/runescape.jar")); // unsigned client output
+		File clientOut = new File("data/game/jaggrab/runescape.pack200");
+		File glClientOut = new File("data/game/jaggrab/runescape_gl.pack200");
+		File joglOut = new File("data/game/jaggrab/jogl.pack200");
+		client.packJar(new File("data/game/jaggrab/runescape.jar")); // unsigned client output
 		client.pack200(clientOut);
 		glClient.pack200(glClientOut);
 		jogl.pack200(joglOut);
@@ -59,42 +59,42 @@ public final class ClientBundler {
 		/* compute SHA1 checksums */
 		logger.info("Computing SHA1 checksums and file sizes...");
 		Resource[] resources = new Resource[] {
-			Resource.create(new File("data/game_unpacker.dat")),
+			Resource.create(new File("data/asm/game_unpacker.dat")),
 			Resource.create(clientOut),
 			Resource.NULL
 		};
 		Resource[] glResources = new Resource[] {
-			Resource.create(new File("data/game_unpacker.dat")),
+			Resource.create(new File("data/asm/game_unpacker.dat")),
 			Resource.create(glClientOut),
 			Resource.NULL,
 			Resource.create(joglOut),
 			Resource.NULL,
-			Resource.create(new File("data/windows-x86/jogl.dll")),
-			Resource.create(new File("data/windows-x86/jogl_awt.dll")),
-			Resource.create(new File("data/windows-amd64/jogl.dll")),
-			Resource.create(new File("data/windows-amd64/jogl_awt.dll")),
-			Resource.create(new File("data/linux-x86/libjogl.so")),
-			Resource.create(new File("data/linux-x86/libjogl_awt.so")),
-			Resource.create(new File("data/linux-x86/libgluegen-rt.so")),
-			Resource.create(new File("data/linux-amd64/libjogl.so")),
-			Resource.create(new File("data/linux-amd64/libjogl_awt.so")),
-			Resource.create(new File("data/linux-amd64/libgluegen-rt.so")),
-			Resource.create(new File("data/mac-ppc/libjogl.jnilib")),
-			Resource.create(new File("data/mac-ppc/libjogl_awt.jnilib")),
-			Resource.create(new File("data/mac-universal/libjogl.jnilib")),
-			Resource.create(new File("data/mac-universal/libjogl_awt.jnilib")),
-			Resource.create(new File("data/solaris-amd64/libjogl.so")),
-			Resource.create(new File("data/solaris-amd64/libjogl_awt.so")),
-			Resource.create(new File("data/solaris-amd64/libgluegen-rt.so")),
-			Resource.create(new File("data/solaris-sparcv9/libjogl.so")),
-			Resource.create(new File("data/solaris-sparcv9/libjogl_awt.so")),
-			Resource.create(new File("data/solaris-sparcv9/libgluegen-rt.so")),
-			Resource.create(new File("data/solaris-sparc/libjogl.so")),
-			Resource.create(new File("data/solaris-sparc/libjogl_awt.so")),
-			Resource.create(new File("data/solaris-sparc/libgluegen-rt.so")),
-			Resource.create(new File("data/solaris-x86/libjogl.so")),
-			Resource.create(new File("data/solaris-x86/libjogl_awt.so")),
-			Resource.create(new File("data/solaris-x86/libgluegen-rt.so"))
+			Resource.create(new File("data/asm/windows-x86/jogl.dll")),
+			Resource.create(new File("data/asm/windows-x86/jogl_awt.dll")),
+			Resource.create(new File("data/asm/windows-amd64/jogl.dll")),
+			Resource.create(new File("data/asm/windows-amd64/jogl_awt.dll")),
+			Resource.create(new File("data/asm/linux-x86/libjogl.so")),
+			Resource.create(new File("data/asm/linux-x86/libjogl_awt.so")),
+			Resource.create(new File("data/asm/linux-x86/libgluegen-rt.so")),
+			Resource.create(new File("data/asm/linux-amd64/libjogl.so")),
+			Resource.create(new File("data/asm/linux-amd64/libjogl_awt.so")),
+			Resource.create(new File("data/asm/linux-amd64/libgluegen-rt.so")),
+			Resource.create(new File("data/asm/mac-ppc/libjogl.jnilib")),
+			Resource.create(new File("data/asm/mac-ppc/libjogl_awt.jnilib")),
+			Resource.create(new File("data/asm/mac-universal/libjogl.jnilib")),
+			Resource.create(new File("data/asm/mac-universal/libjogl_awt.jnilib")),
+			Resource.create(new File("data/asm/solaris-amd64/libjogl.so")),
+			Resource.create(new File("data/asm/solaris-amd64/libjogl_awt.so")),
+			Resource.create(new File("data/asm/solaris-amd64/libgluegen-rt.so")),
+			Resource.create(new File("data/asm/solaris-sparcv9/libjogl.so")),
+			Resource.create(new File("data/asm/solaris-sparcv9/libjogl_awt.so")),
+			Resource.create(new File("data/asm/solaris-sparcv9/libgluegen-rt.so")),
+			Resource.create(new File("data/asm/solaris-sparc/libjogl.so")),
+			Resource.create(new File("data/asm/solaris-sparc/libjogl_awt.so")),
+			Resource.create(new File("data/asm/solaris-sparc/libgluegen-rt.so")),
+			Resource.create(new File("data/asm/solaris-x86/libjogl.so")),
+			Resource.create(new File("data/asm/solaris-x86/libjogl_awt.so")),
+			Resource.create(new File("data/asm/solaris-x86/libgluegen-rt.so"))
 		};
 
 		/* update SHA1 checksums in the loader */
@@ -112,43 +112,43 @@ public final class ClientBundler {
 
 		/* write the loaders back out */
 		logger.info("Bundling loaders...");
-		loader.packJar(new File("../game/data/www/loader.jar"));
-		glLoader.packJar(new File("../game/data/www/loader_gl.jar"));
+		loader.packJar(new File("data/game/jaggrab/loader.jar"));
+		glLoader.packJar(new File("data/game/jaggrab/loader_gl.jar"));
 
 		/* copy natives/unpacker */
 		logger.info("Copying native libraries and game unpacker...");
-		copy("data/game_unpacker.dat", "../game/data/www/unpackclass.pack");
-		copy("data/windows-x86/jogl.dll", "../game/data/www/jogl_0_0.lib");
-		copy("data/windows-x86/jogl_awt.dll", "../game/data/www/jogl_0_1.lib");
-		copy("data/windows-amd64/jogl.dll", "../game/data/www/jogl_1_0.lib");
-		copy("data/windows-amd64/jogl_awt.dll", "../game/data/www/jogl_1_1.lib");
-		copy("data/linux-x86/libjogl.so", "../game/data/www/jogl_2_0.lib");
-		copy("data/linux-x86/libjogl_awt.so", "../game/data/www/jogl_2_1.lib");
-		copy("data/linux-x86/libgluegen-rt.so", "../game/data/www/jogl_2_2.lib");
-		copy("data/linux-amd64/libjogl.so", "../game/data/www/jogl_3_0.lib");
-		copy("data/linux-amd64/libjogl_awt.so", "../game/data/www/jogl_3_1.lib");
-		copy("data/linux-amd64/libgluegen-rt.so", "../game/data/www/jogl_3_2.lib");
-		copy("data/mac-ppc/libjogl.jnilib", "../game/data/www/jogl_4_0.lib");
-		copy("data/mac-ppc/libjogl_awt.jnilib", "../game/data/www/jogl_4_1.lib");
-		copy("data/mac-universal/libjogl.jnilib", "../game/data/www/jogl_5_0.lib");
-		copy("data/mac-universal/libjogl_awt.jnilib", "../game/data/www/jogl_5_1.lib");
-		copy("data/solaris-amd64/libjogl.so", "../game/data/www/jogl_6_0.lib");
-		copy("data/solaris-amd64/libjogl_awt.so", "../game/data/www/jogl_6_1.lib");
-		copy("data/solaris-amd64/libgluegen-rt.so", "../game/data/www/jogl_6_2.lib");
-		copy("data/solaris-sparcv9/libjogl.so", "../game/data/www/jogl_7_0.lib");
-		copy("data/solaris-sparcv9/libjogl_awt.so", "../game/data/www/jogl_7_1.lib");
-		copy("data/solaris-sparcv9/libgluegen-rt.so", "../game/data/www/jogl_7_2.lib");
-		copy("data/solaris-sparc/libjogl.so", "../game/data/www/jogl_8_0.lib");
-		copy("data/solaris-sparc/libjogl_awt.so", "../game/data/www/jogl_8_1.lib");
-		copy("data/solaris-sparc/libgluegen-rt.so", "../game/data/www/jogl_8_2.lib");
-		copy("data/solaris-x86/libjogl.so", "../game/data/www/jogl_9_0.lib");
-		copy("data/solaris-x86/libjogl_awt.so", "../game/data/www/jogl_9_1.lib");
-		copy("data/solaris-x86/libgluegen-rt.so", "../game/data/www/jogl_9_2.lib");
+		copy("data/asm/game_unpacker.dat", "data/game/jaggrab/unpackclass.pack");
+		copy("data/asm/windows-x86/jogl.dll", "data/game/jaggrab/jogl_0_0.lib");
+		copy("data/asm/windows-x86/jogl_awt.dll", "data/game/jaggrab/jogl_0_1.lib");
+		copy("data/asm/windows-amd64/jogl.dll", "data/game/jaggrab/jogl_1_0.lib");
+		copy("data/asm/windows-amd64/jogl_awt.dll", "data/game/jaggrab/jogl_1_1.lib");
+		copy("data/asm/linux-x86/libjogl.so", "data/game/jaggrab/jogl_2_0.lib");
+		copy("data/asm/linux-x86/libjogl_awt.so", "data/game/jaggrab/jogl_2_1.lib");
+		copy("data/asm/linux-x86/libgluegen-rt.so", "data/game/jaggrab/jogl_2_2.lib");
+		copy("data/asm/linux-amd64/libjogl.so", "data/game/jaggrab/jogl_3_0.lib");
+		copy("data/asm/linux-amd64/libjogl_awt.so", "data/game/jaggrab/jogl_3_1.lib");
+		copy("data/asm/linux-amd64/libgluegen-rt.so", "data/game/jaggrab/jogl_3_2.lib");
+		copy("data/asm/mac-ppc/libjogl.jnilib", "data/game/jaggrab/jogl_4_0.lib");
+		copy("data/asm/mac-ppc/libjogl_awt.jnilib", "data/game/jaggrab/jogl_4_1.lib");
+		copy("data/asm/mac-universal/libjogl.jnilib", "data/game/jaggrab/jogl_5_0.lib");
+		copy("data/asm/mac-universal/libjogl_awt.jnilib", "data/game/jaggrab/jogl_5_1.lib");
+		copy("data/asm/solaris-amd64/libjogl.so", "data/game/jaggrab/jogl_6_0.lib");
+		copy("data/asm/solaris-amd64/libjogl_awt.so", "data/game/jaggrab/jogl_6_1.lib");
+		copy("data/asm/solaris-amd64/libgluegen-rt.so", "data/game/jaggrab/jogl_6_2.lib");
+		copy("data/asm/solaris-sparcv9/libjogl.so", "data/game/jaggrab/jogl_7_0.lib");
+		copy("data/asm/solaris-sparcv9/libjogl_awt.so", "data/game/jaggrab/jogl_7_1.lib");
+		copy("data/asm/solaris-sparcv9/libgluegen-rt.so", "data/game/jaggrab/jogl_7_2.lib");
+		copy("data/asm/solaris-sparc/libjogl.so", "data/game/jaggrab/jogl_8_0.lib");
+		copy("data/asm/solaris-sparc/libjogl_awt.so", "data/game/jaggrab/jogl_8_1.lib");
+		copy("data/asm/solaris-sparc/libgluegen-rt.so", "data/game/jaggrab/jogl_8_2.lib");
+		copy("data/asm/solaris-x86/libjogl.so", "data/game/jaggrab/jogl_9_0.lib");
+		copy("data/asm/solaris-x86/libjogl_awt.so", "data/game/jaggrab/jogl_9_1.lib");
+		copy("data/asm/solaris-x86/libgluegen-rt.so", "data/game/jaggrab/jogl_9_2.lib");
 
 		/* sign loaders */
 		logger.info("Signing loaders...");
-		Runtime.getRuntime().exec("jarsigner -keystore ../util/data/scapeemu.keystore -storepass scapeemu ../game/data/www/loader.jar scapeemu");
-		Runtime.getRuntime().exec("jarsigner -keystore ../util/data/scapeemu.keystore -storepass scapeemu ../game/data/www/loader_gl.jar scapeemu");
+		Runtime.getRuntime().exec("jarsigner -keystore data/asm/scapeemu.keystore -storepass scapeemu data/game/jaggrab/loader.jar scapeemu");
+		Runtime.getRuntime().exec("jarsigner -keystore data/asm/scapeemu.keystore -storepass scapeemu data/game/jaggrab/loader_gl.jar scapeemu");
 
 		/* all done! */
 		logger.info("Client bundler completed successfully.");
