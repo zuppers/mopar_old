ad8fb6d1f60e25a80bc1db6f6a7e8fc3ad4d0583
Davidi2 <davidtheinsley@comcast.net>
2015-01-22 12:54:50 -0800

    Fixed login packets being sent out of order (fixes main crash)

--- src/net/scapeemulator/game/net/game/GameSession.java
+++ src/net/scapeemulator/game/net/game/GameSession.java
@@ -2,6 +2,7 @@ package net.scapeemulator.game.net.game;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 
 import java.io.IOException;
 import java.util.ArrayDeque;
@@ -14,68 +15,84 @@ import net.scapeemulator.game.model.player.PlayerOption;
 import net.scapeemulator.game.msg.Message;
 import net.scapeemulator.game.msg.MessageDispatcher;
 import net.scapeemulator.game.msg.impl.RegionChangeMessage;
+import net.scapeemulator.game.net.RsChannelHandler;
 import net.scapeemulator.game.net.Session;
 
 public final class GameSession extends Session {
 
-	private final Player player;
-	private final MessageDispatcher dispatcher;
-	private final Queue<Message> messages = new ArrayDeque<>();
+    private final Player player;
+    private final MessageDispatcher dispatcher;
+    private final Queue<Message> messages = new ArrayDeque<>();
 
-	public GameSession(GameServer server, Channel channel, Player player) {
-		super(server, channel);
-		this.player = player;
-		this.dispatcher = server.getMessageDispatcher();
-	}
+    public GameSession(GameServer server, Channel channel, Player player) {
+        super(server, channel);
+        this.player = player;
+        this.dispatcher = server.getMessageDispatcher();
+    }
 
-	public void init() {
-		player.setSession(this);
+    public void init() {
+        /* set up player for their initial region change */
+        Position position = player.getPosition();
+        player.setLastKnownRegion(position);
 
-		/* set up player for their initial region change */
-		Position position = player.getPosition();
-		player.setLastKnownRegion(position);
-		player.send(new RegionChangeMessage(player.getPosition()));
+        ChannelFuture future = channel.write(new RegionChangeMessage(position));
 
-		/* set up the game interface */
-		player.getInterfaceSet().init();
-		player.getPlayerCombatHandler().init();
-		
-		/* Set all the default options that all players have available */
-		player.getOption(PlayerOption.FOLLOW_OPTION).setText("Follow");
-		player.getOption(PlayerOption.TRADE_OPTION).setText("Trade with");
-		player.refreshOptions();
+        future.addListener(new ChannelFutureListener() {
+            public void operationComplete(ChannelFuture future) {
+                if (!future.isSuccess()) {
+                    System.err.println(channel.pipeline().get(RsChannelHandler.class));
+                    channel.disconnect();
+                } else {
+                    startSession();
+                }
+            }
+        });
+    }
 
-		/* fireEvents skills, inventory, energy, etc. */
-		player.getInventory().refresh();
-		player.getEquipment().refresh();		
-		player.getSettings().refresh();
-		player.getSkillSet().refresh();
-		player.setEnergy(player.getEnergy());
-		player.onLogin();
-	}
+    private void startSession() {
+        player.setSession(this);
 
-	@Override
-	public void messageReceived(Object message) throws IOException {
-		synchronized (messages) {
-			messages.add((Message) message);
-		}
-	}
+        /* set up the game interface */
+        player.getInterfaceSet().init();
 
-	@Override
-	public void channelClosed() {
-		server.getLoginService().addLogoutRequest(player);
-	}
+        player.getPlayerCombatHandler().init();
 
-	public ChannelFuture send(Message message) {
-		return channel.write(message);
-	}
+        /* Set all the default options that all players have available */
+        player.getOption(PlayerOption.FOLLOW_OPTION).setText("Follow");
+        player.getOption(PlayerOption.TRADE_OPTION).setText("Trade with");
+        player.refreshOptions();
 
-	public void processMessageQueue() {
-		synchronized (messages) {
-			Message message;
-			while ((message = messages.poll()) != null)
-				dispatcher.dispatch(player, message);
-		}
-	}
+        /* fireEvents skills, inventory, energy, etc. */
+        player.getInventory().refresh();
+        player.getEquipment().refresh();
+        player.getSettings().refresh();
+        player.getSkillSet().refresh();
+        player.setEnergy(player.getEnergy());
+        player.onLogin();
+    }
+
+    @Override
+    public void messageReceived(Object message) throws IOException {
+        synchronized (messages) {
+            messages.add((Message) message);
+        }
+    }
+
+    @Override
+    public void channelClosed() {
+        server.getLoginService().addLogoutRequest(player);
+    }
+
+    public ChannelFuture send(Message message) {
+        return channel.write(message);
+    }
+
+    public void processMessageQueue() {
+        synchronized (messages) {
+            Message message;
+            while ((message = messages.poll()) != null)
+                dispatcher.dispatch(player, message);
+        }
+    }
 
 }
--- src/net/scapeemulator/game/net/login/LoginSession.java
+++ src/net/scapeemulator/game/net/login/LoginSession.java
@@ -2,13 +2,13 @@ package net.scapeemulator.game.net.login;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelPipeline;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 
 import java.io.IOException;
 import java.security.SecureRandom;
-
 import net.burtleburtle.bob.rand.IsaacRandom;
 import net.scapeemulator.cache.ChecksumTable;
 import net.scapeemulator.game.GameServer;
@@ -24,107 +24,100 @@ import net.scapeemulator.game.net.game.GameSession;
 
 public final class LoginSession extends Session {
 
-	private static final SecureRandom random = new SecureRandom();
+    private static final SecureRandom random = new SecureRandom();
 
-	private final LoginService service;
-	private final long serverSessionKey = random.nextLong();
-	private int displayMode;
-	private IsaacRandom inRandom, outRandom;
+    private final LoginService service;
+    private final long serverSessionKey = random.nextLong();
+    private int displayMode;
+    private IsaacRandom inRandom, outRandom;
 
-	public LoginSession(GameServer server, Channel channel) {
-		super(server, channel);
-		this.service = server.getLoginService();
-		init();
-	}
+    public LoginSession(GameServer server, Channel channel) {
+        super(server, channel);
+        this.service = server.getLoginService();
+        init();
+    }
 
-	private void init() {
-		ByteBuf buf = channel.alloc().buffer(8);
-		buf.writeLong(serverSessionKey);
-		channel.write(new LoginResponse(LoginResponse.STATUS_EXCHANGE_KEYS, buf));
-	}
+    private void init() {
+        ByteBuf buf = channel.alloc().buffer(8);
+        buf.writeLong(serverSessionKey);
+        channel.write(new LoginResponse(LoginResponse.STATUS_EXCHANGE_KEYS, buf));
+    }
 
-	public void sendLoginFailure(int status) {
-		LoginResponse response = new LoginResponse(status);
-		channel.write(response).addListener(ChannelFutureListener.CLOSE);
-	}
+    public void sendLoginFailure(int status) {
+        LoginResponse response = new LoginResponse(status);
+        channel.write(response).addListener(ChannelFutureListener.CLOSE);
+    }
 
-	public void sendLoginSuccess(int status, Player player) {
-		ByteBuf buf = channel.alloc().buffer(11);
-		buf.writeByte(player.getRights());
-		buf.writeByte(0);
-		buf.writeByte(0);
-		buf.writeByte(0);
-		buf.writeByte(0);
-		buf.writeByte(0);
-		buf.writeByte(0);
-		buf.writeShort(player.getId());
-		buf.writeByte(1);
-		buf.writeByte(1);
+    public void sendLoginSuccess(int status, final Player player) {
+        ByteBuf buf = channel.alloc().buffer(11);
+        buf.writeByte(player.getRights());
+        buf.writeByte(0);
+        buf.writeByte(0);
+        buf.writeByte(0);
+        buf.writeByte(0);
+        buf.writeByte(0);
+        buf.writeByte(0);
+        buf.writeShort(player.getId());
+        buf.writeByte(1);
+        buf.writeByte(1);
+        if (displayMode == 0 || displayMode == 1) {
+            player.getInterfaceSet().setDisplayMode(DisplayMode.FIXED);
+        } else {
+            player.getInterfaceSet().setDisplayMode(DisplayMode.RESIZABLE);
+        }
+        final ChannelPipeline pipeline = channel.pipeline();
+        final GameSession session = new GameSession(server, channel, player);
+        RsChannelHandler handler = pipeline.get(RsChannelHandler.class);
+        handler.setSession(session);
+        pipeline.remove(ReadTimeoutHandler.class);
+        LoginResponse response = new LoginResponse(status, buf);
+        ChannelFuture future = channel.write(response);
+        future.addListener(new ChannelFutureListener() {
+            public void operationComplete(ChannelFuture future) {
+                if (future.isSuccess()) {
+                    pipeline.addFirst(new GameFrameEncoder(outRandom), new GameMessageEncoder(server.getCodecRepository()), new GameFrameDecoder(inRandom),
+                            new GameMessageDecoder(server.getCodecRepository()));
 
-		ChannelPipeline pipeline = channel.pipeline();
+                    session.init();
+                }
+            }
+        });
+    }
 
-		GameSession session = new GameSession(server, channel, player);
-		RsChannelHandler handler = pipeline.get(RsChannelHandler.class);
-		handler.setSession(session);
+    @Override
+    public void messageReceived(Object message) throws IOException {
+        LoginRequest request = (LoginRequest) message;
+        if (request.getServerSessionKey() != serverSessionKey)
+            throw new IOException("Server session key mismatch.");
+        boolean versionMismatch = false;
+        if (request.getVersion() != server.getVersion())
+            versionMismatch = true;
+        ChecksumTable table = server.getChecksumTable();
+        int[] crc = request.getCrc();
+        for (int i = 0; i < crc.length; i++) {
+            if (table.getEntry(i).getCrc() != crc[i]) {
+                versionMismatch = true;
+                break;
+            }
+        }
+        if (versionMismatch) {
+            sendLoginFailure(LoginResponse.STATUS_GAME_UPDATED);
+            return;
+        }
+        long clientSessionKey = request.getClientSessionKey();
+        long serverSessionKey = request.getServerSessionKey();
+        int[] seed = new int[4];
+        seed[0] = (int) (clientSessionKey >> 32);
+        seed[1] = (int) clientSessionKey;
+        seed[2] = (int) (serverSessionKey >> 32);
+        seed[3] = (int) serverSessionKey;
+        inRandom = new IsaacRandom(seed);
+        for (int i = 0; i < seed.length; i++)
+            seed[i] += 50;
+        outRandom = new IsaacRandom(seed);
+        displayMode = request.getDisplayMode();
 
-		pipeline.remove(ReadTimeoutHandler.class); // TODO a different timeout mechanism is used in-game
-
-		LoginResponse response = new LoginResponse(status, buf);
-		channel.write(response);
-
-		pipeline.addFirst(
-			new GameFrameEncoder(outRandom),
-			new GameMessageEncoder(server.getCodecRepository()),
-			new GameFrameDecoder(inRandom),
-			new GameMessageDecoder(server.getCodecRepository()));
-
-		if (displayMode == 0 || displayMode == 1)
-			player.getInterfaceSet().setDisplayMode(DisplayMode.FIXED);
-		else
-			player.getInterfaceSet().setDisplayMode(DisplayMode.RESIZABLE);
-
-		session.init();
-	}
-
-	@Override
-	public void messageReceived(Object message) throws IOException {
-		LoginRequest request = (LoginRequest) message;
-		if (request.getServerSessionKey() != serverSessionKey)
-			throw new IOException("Server session key mismatch.");
-
-		boolean versionMismatch = false;
-		if (request.getVersion() != server.getVersion())
-			versionMismatch = true;
-
-		ChecksumTable table = server.getChecksumTable();
-		int[] crc = request.getCrc();
-		for (int i = 0; i < crc.length; i++) {
-			if (table.getEntry(i).getCrc() != crc[i]) {
-				versionMismatch = true;
-				break;
-			}
-		}
-
-		if (versionMismatch) {
-			sendLoginFailure(LoginResponse.STATUS_GAME_UPDATED);
-			return;
-		}
-
-		long clientSessionKey = request.getClientSessionKey();
-		long serverSessionKey = request.getServerSessionKey();
-		int[] seed = new int[4];
-		seed[0] = (int) (clientSessionKey >> 32);
-		seed[1] = (int) clientSessionKey;
-		seed[2] = (int) (serverSessionKey >> 32);
-		seed[3] = (int) serverSessionKey;
-
-		inRandom = new IsaacRandom(seed);
-		for (int i = 0; i < seed.length; i++)
-			seed[i] += 50;
-		outRandom = new IsaacRandom(seed);
-		displayMode = request.getDisplayMode();
-
-		service.addLoginRequest(this, request);
-	}
+        service.addLoginRequest(this, request);
+    }
 
 }
