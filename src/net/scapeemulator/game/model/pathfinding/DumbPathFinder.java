c864b3f8ff0b3f78f4c59a1ccf47dbdb07215dae
Davidi2 <davidtheinsley@comcast.net>
2015-01-23 21:54:46 -0800

    Dumb path finder can now check if the mob would move into the destination, and prevent it.

--- src/net/scapeemulator/game/model/pathfinding/DumbPathFinder.java
+++ src/net/scapeemulator/game/model/pathfinding/DumbPathFinder.java
@@ -5,12 +5,57 @@ import net.scapeemulator.game.model.mob.Direction;
 
 public class DumbPathFinder {
 
-    public static Path find(Position position, Position dest, int size, int max) {
+    public static Path find(Position position, Position dest, int size, int max, boolean inside) {
         Path path = new Path();
         Position p = position;
         for (int i = 0; i < max; i++) {
             p = bestDummyPosition(p, dest, size);
             if (p != null) {
+                if (!inside && p.equals(dest)) {
+                    Position cur;
+                    if (path.isEmpty()) {
+                        cur = position;
+                    } else {
+                        cur = path.getPoints().getLast();
+                    }
+                    switch (Direction.between(cur, dest)) {
+                    case NONE:
+                    case EAST:
+                    case NORTH:
+                    case SOUTH:
+                    case WEST:
+                        break;
+                    case NORTH_EAST:
+                        if (Direction.isTraversable(cur, Direction.NORTH, size)) {
+                            path.addLast(new Position(cur.getX(), cur.getY() + 1, cur.getHeight()));
+                        } else if (Direction.isTraversable(cur, Direction.EAST, size)) {
+                            path.addLast(new Position(cur.getX() + 1, cur.getY(), cur.getHeight()));
+                        }
+                        break;
+                    case NORTH_WEST:
+                        if (Direction.isTraversable(cur, Direction.NORTH, size)) {
+                            path.addLast(new Position(cur.getX(), cur.getY() + 1, cur.getHeight()));
+                        } else if (Direction.isTraversable(cur, Direction.WEST, size)) {
+                            path.addLast(new Position(cur.getX() - 1, cur.getY(), cur.getHeight()));
+                        }
+                        break;
+                    case SOUTH_EAST:
+                        if (Direction.isTraversable(cur, Direction.SOUTH, size)) {
+                            path.addLast(new Position(cur.getX(), cur.getY() - 1, cur.getHeight()));
+                        } else if (Direction.isTraversable(cur, Direction.EAST, size)) {
+                            path.addLast(new Position(cur.getX() + 1, cur.getY(), cur.getHeight()));
+                        }
+                        break;
+                    case SOUTH_WEST:
+                        if (Direction.isTraversable(cur, Direction.SOUTH, size)) {
+                            path.addLast(new Position(cur.getX(), cur.getY() - 1, cur.getHeight()));
+                        } else if (Direction.isTraversable(cur, Direction.WEST, size)) {
+                            path.addLast(new Position(cur.getX() - 1, cur.getY(), cur.getHeight()));
+                        }
+                        break;
+                    }
+                    return path;
+                }
                 path.addLast(p);
             } else {
                 return path;
