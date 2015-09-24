c253c700643160318d85573ff59bc8dc409d5b22
Davidi2 <davidtheinsley@comcast.net>
2015-09-24 15:32:48 -0700

    Added SkillRequirement xp reward

--- src/net/scapeemulator/game/model/player/requirement/SkillRequirement.java
+++ src/net/scapeemulator/game/model/player/requirement/SkillRequirement.java
@@ -13,11 +13,17 @@ public class SkillRequirement extends Requirement {
     private final int level;
     private final String error;
     private final boolean current;
+    private final double xpReward;
 
     public SkillRequirement(int skill, int level, boolean current, String concat) {
+        this(skill, level, current, concat, 0);
+    }
+
+    public SkillRequirement(int skill, int level, boolean current, String concat, double xpReward) {
         this.skill = skill;
         this.level = level;
         this.current = current;
+        this.xpReward = xpReward;
         error = "You need level " + level + " " + StringUtils.capitalize(Skill.SKILL_NAMES[skill]) + " to " + concat + ".";
     }
 
@@ -38,6 +44,9 @@ public class SkillRequirement extends Requirement {
 
     @Override
     public void fulfill(Player player) {
+        if (xpReward > 0) {
+            player.getSkillSet().addExperience(skill, xpReward);
+        }
     }
 
 }
