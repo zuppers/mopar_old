aec7b85a63e004f07fa465709be6b9893334eedb
Davidi2 <davidtheinsley@comcast.net>
2016-04-05 13:14:49 -0700

    Added missing SkillRecipe class for MakeItemInterface

--- src/net/scapeemulator/game/model/player/skills/SkillRecipe.java
+++ src/net/scapeemulator/game/model/player/skills/SkillRecipe.java
@@ -1,10 +1,17 @@
 package net.scapeemulator.game.model.player.skills;
 
 import net.scapeemulator.game.model.player.Item;
+import net.scapeemulator.game.model.player.requirement.Requirements;
 
 public abstract class SkillRecipe {
 
-    Item getProduct() {
+    protected Requirements requirements;
+    
+    public Item getProduct() {
         return null;
+    }
+    
+    public Requirements getRequirements() {
+        return requirements;
     }
 }
